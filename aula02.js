ba4f5dc538d7a08b72d527abdf3a7bfc7d91dfd8
Midoriya <victornascimentobr48@gmail.com>
2026-02-02 22:46:36 -0300

    feat: definindo strings

--- aula02.js
+++ aula02.js
@@ -3,10 +3,27 @@
 // window.confirm("Caixa de confirmação")
 // window.prompt("Caixa de entrada de dados")
 
+// No java script usamos let ou var para declarar variaveis
+
 var n1 = 5
 var n2 = 6
 var n3 = 7
 
+let n4 = 8
+let n5 = 9
+let n6 = 10
+
+// Exemplos definção de strings
+
+var s1 = "Bom dia"
+var s2 = "Boa tarde"
+var s3 = "Boa noite"
+
+let s4 = "Bom dia"
+let s5 = "Boa tarde"
+let s6 = "Boa noite"
+
 var s = n1 + n2 + n3
 
 console.log(s)
+
