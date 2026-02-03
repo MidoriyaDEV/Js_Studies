4282ae759a50f23574f2fb52bb0479d5d1c1283b
Midoriya <victornascimentobr48@gmail.com>
2026-02-02 22:39:01 -0300

    feat: criando variaveis

---
+++ aula02.js
@@ -0,0 +1,12 @@
+// AULA ANTERIOR
+// window.alert("Caixa de mensagem")
+// window.confirm("Caixa de confirmação")
+// window.prompt("Caixa de entrada de dados")
+
+var n1 = 5
+var n2 = 6
+var n3 = 7
+
+var s = n1 + n2 + n3
+
+console.log(s)
