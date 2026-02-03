28598fa58b28e139eb4531a42f8b225fd7a5fe29
Midoriya <victornascimentobr48@gmail.com>
2026-02-02 22:54:51 -0300

    feat: Regras/identificadores

--- aula02.js
+++ aula02.js
@@ -4,6 +4,7 @@
 // window.prompt("Caixa de entrada de dados")
 
 // No java script usamos let ou var para declarar variaveis
+// o que é uma variavel? Basicamente é umespaço de memória que recebe um valor
 
 var n1 = 5
 var n2 = 6
@@ -24,6 +25,13 @@ let s5 = "Boa tarde"
 let s6 = "Boa noite"
 
 var s = n1 + n2 + n3
+
+// regras dos identificadores
+
+// 1 - Nao pode comecar com numeros
+// 2 - Nao pode conter espacos
+// 3 - Nao pode conter caracteres especiais
+// 4 - Nao pode ser uma palavra reservada
 
 console.log(s)
 
